8e19d80db34038c22282ec41e73eb0e0c80e2096
NamanKothari5 <namankothari0512@gmail.com>
2024-04-04 16:45:49 +0530

    added initial frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,88 @@
-import { Toaster } from "react-hot-toast";
-import Login from "./pages/login";
-import { useDispatch, useSelector } from "react-redux";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { userExist, userNotExist } from "./redux/reducer/userReducer";
-import { auth } from "./firebase";
-import { getUser } from "./redux/api/userAPI";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
-
-
+import Home from './pages/home/Home';
+import Order from './pages/order/Order';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
+import MyState from './context/data/myState';
+import Login from './pages/registration/Login';
+import Signup from './pages/registration/Signup';
+import ProductInfo from './pages/productInfo/ProductInfo';
+import AddProduct from './pages/admin/page/AddProduct';
+import UpdateProduct from './pages/admin/page/UpdateProduct';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Allproducts from './pages/allproducts/Allproducts';
 function App() {
-  const { user, loading } = useSelector(
-    (state) =>
-    state.userReducer
-  );
-  const dispatch = useDispatch();
-  useEffect(() => {
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const data = await getUser(user.uid);
-        if(data.user)
-        dispatch(userExist(data.user));
-        else
-        dispatch(userNotExist());
-      }
-      else {
-        dispatch(userNotExist());
-      }
-    });
-  }, []);
-
   return (
-    <div>
-    <Login></Login>
-    <Toaster />
-    
-    </div>
-  );
+    <MyState>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/allproducts" element={<Allproducts />} />
+          <Route path="/order" element={
+            <ProtectedRoute>
+              <Order />
+            </ProtectedRoute>
+          } />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/dashboard" element={
+            <ProtectedRouteForAdmin>
+              <Dashboard />
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/productinfo/:id' element={<ProductInfo/>} />
+          <Route path='/addproduct' element={
+            <ProtectedRouteForAdmin>
+              <AddProduct/>
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path='/updateproduct' element={
+            <ProtectedRouteForAdmin>
+              <UpdateProduct/>
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+        <ToastContainer/>
+      </Router>
+    </MyState>
+
+  )
 }
 
-export default App;
+export default App 
+
+// user 
+
+export const ProtectedRoute = ({children}) => {
+  const user = localStorage.getItem('user')
+  if(user){
+    return children
+  }else{
+    return <Navigate to={'/login'}/>
+  }
+}
+
+// admin 
+
+const ProtectedRouteForAdmin = ({children})=> {
+  const admin = JSON.parse(localStorage.getItem('user'))
+  
+  if(admin.user.email === 'knupadhyay784@gmail.com'){
+    return children
+  }
+  else{
+    return <Navigate to={'/login'}/>
+  }
+
+}
