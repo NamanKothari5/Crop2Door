6c211a072d2c4c7bb94e81deb56ab1714d71837d
Bhavik Bharambe <bhavik.bharambe02@gmail.com>
2024-04-08 09:25:44 +0530

    Backend done

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -7,119 +7,167 @@ const { Product } = require("../models/product");
 const { generateClusters, findCluster } = require("../utils/mapBoxUtils");
 
 module.exports.newOrder = TryCatch(async (req, res, next) => {
-    const { id } = req.query;
-    const { orderItems } = req.body;
+  const { id } = req.query;
+  const { orderItems } = req.body;
 
-    let totalPrice = 0;
-    let requiredProducts = {};
-    for (let i = 0; i < orderItems.length; i++) {
-        let currQuantity = Number(orderItems[i].quantity);
-        let currPrice = Number(orderItems[i].price);
-        totalPrice += currQuantity * currPrice;
+  let totalPrice = 0;
+  let requiredProducts = {};
+  for (let i = 0; i < orderItems.length; i++) {
+    let currQuantity = Number(orderItems[i].quantity);
+    let currPrice = Number(orderItems[i].price);
+    totalPrice += currQuantity * currPrice;
+  }
+  requiredProducts = orderItems.reduce((obj, item) => {
+    obj[item.name] = item.quantity;
+    return obj;
+  }, {});
+
+  // console.log(products)
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  const currentDate = `${day}-${month}-${year}`;
+  const order = await Order.create({
+    user: id,
+    orderItems,
+    totalPrice,
+    date: currentDate,
+  });
+
+  const user = await User.findById(id);
+  user.orders.push(order._id);
+  await user.save();
+
+  
+
+  const coordinates = user.coordinates;
+
+  const allFarms = await Farm.find({});
+
+  let query = `${coordinates[0]},${coordinates[1]}`;
+
+  allFarms.forEach((farm) => {
+    const farmLongitude = farm.coordinates[0];
+    const farmLatitude = farm.coordinates[1];
+    query += `;${farmLongitude},${farmLatitude}`;
+  });
+  const baseURl = process.env.MAPBOX_BASE_URL;
+  const token = process.env.MAPBOX_TOKEN;
+
+  let response = await fetch(
+    `${baseURl}/directions-matrix/v1/mapbox/driving/${query}?sources=0&annotations=distance,duration&access_token=${token}`
+  );
+
+  if (!response.ok)
+    return next(new CustomError("Error retreiving appropriate products", 400));
+
+  let jsonData = await response.json();
+  let distances = jsonData.distances[0];
+
+  let validFarms = [];
+  for (let idx = 1; idx < distances.length; idx++) {
+    if (distances[idx] <= 200000) validFarms.push(allFarms[idx - 1]);
+  }
+
+  console.log(validFarms);
+
+  let produce = validFarms.map((e) => {
+    return {};
+  });
+  for (let i = 0; i < validFarms.length; i++) {
+    for (let j = 0; j < validFarms[i].products.length; j++) {
+      const productId = validFarms[i].products[j];
+      const product = await Product.findById(productId);
+      produce[i][product.name] = product.stock;
     }
-    requiredProducts = orderItems.reduce((obj, item) => {
-        obj[item.name] = item.quantity;
-        return obj;
-    }, {})
-    
-    // console.log(products)
-    const date = new Date();
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
+  }
+  let currClusterQuantity = {};
+  let allClusters = [];
+  let currCluster = [validFarms.length];
 
-    const currentDate = `${day}-${month}-${year}`;
-    const order = await Order.create({
-        user: id,
-        orderItems,
-        totalPrice,
-        date: currentDate
-    });
+  for(const crop in requiredProducts)
+    currClusterQuantity[crop] = 0;
 
-    const user = await User.findById(id);
-    user.orders.push(order._id);
-    await user.save();
+  const addresses = validFarms.map((farm) => farm.coordinates);
+  addresses.push(coordinates);
+  query = "";
+  addresses.forEach((coordinates) => {
+    const farmLongitude = coordinates[0];
+    const farmLatitude = coordinates[1];
+    query += `${farmLongitude},${farmLatitude};`;
+  });
+  query = query.slice(0, -1);
 
-    const coordinates = user.coordinates;
+  response = await fetch(
+    `${baseURl}/directions-matrix/v1/mapbox/driving/${query}?annotations=distance&access_token=${token}`
+  );
 
-    const allFarms = await Farm.find({});
-
-    let query = `${coordinates[0]},${coordinates[1]}`;
-
-    allFarms.forEach((farm) => {
-        const farmLongitude = farm.coordinates[0];
-        const farmLatitude = farm.coordinates[1];
-        query += `;${farmLongitude},${farmLatitude}`;
-    });
-    const baseURl = process.env.MAPBOX_BASE_URL;
-    const token = process.env.MAPBOX_TOKEN;
-
-    let response = await fetch(`${baseURl}/directions-matrix/v1/mapbox/driving/${query}?sources=0&annotations=distance,duration&access_token=${token}`);
-
-    if (!response.ok)
-        return next(new CustomError("Error retreiving appropriate products", 400));
-
-    let jsonData = await response.json();
-    let distances = jsonData.distances[0];
-
-    let validFarms = [];
-    for (let idx = 1; idx < distances.length; idx++) {
-        if (distances[idx] <= 200000)
-            validFarms.push(allFarms[idx - 1]);
-    }
-
-    let produce = validFarms.map((e) => {
-        return {};
-    });
-    for(let i=0; i<validFarms.length; i++){
-        for(let j=0; j<validFarms[i].products.length; j++){
-            const productId = validFarms[i].products[j];
-            const product = await Product.findById(productId);
-            produce[i][product.name] = product.stock;
-        }
-    }
-    let currClusterQuantity = {};
-    let allClusters = [];
-    let currCluster = [validFarms.length];
-
-    const addresses = validFarms.map((farm)=>farm.coordinates);
-    addresses.push(coordinates);
-    query="";
-    addresses.forEach((coordinates) => {
-        const farmLongitude = coordinates[0];
-        const farmLatitude = coordinates[1];
-        query += `${farmLongitude},${farmLatitude};`;
-    });
-    query = query.slice(0, -1);
-    
-    response=await fetch(`${baseURl}/directions-matrix/v1/mapbox/driving/${query}?annotations=distance&access_token=${token}`);
-
-    if(!response.ok)
+  if (!response.ok)
     return next(new CustomError("Error retreiving farm distances", 400));
 
-    jsonData=await response.json();
-    distances = jsonData.distances;
+  jsonData = await response.json();
+  distances = jsonData.distances;
 
-    console.log(distances);
-    generateClusters(requiredProducts, produce, 0, currClusterQuantity, currCluster, allClusters);
+  console.log(distances);
 
-    const {min_dist, finalPath} = findCluster(distances, allClusters);
-    console.log(min_dist);
-    console.log(finalPath);
+  generateClusters(
+    requiredProducts,
+    produce,
+    0,
+    currClusterQuantity,
+    currCluster,
+    allClusters
+  );
 
-    return res.status(200).json({
-        success: true,
-        message: "Order Created Successfully"
-    });
-})
+  console.log(allClusters);
 
-module.exports.myOrders = TryCatch(async (req, res, nect) => {
-    const { id } = req.query;
-    const user = await User.findById(id).populate("orders");
+  let { min_dist, finalPath } = findCluster(distances, allClusters);
 
-    return res.status(200).json({
-        success: true,
-        orders:user.orders
-    });
+  console.log(finalPath);
+
+  finalPath=finalPath.map((idx)=>{
+    if(idx<validFarms.length){
+        const farm = validFarms[idx];
+        return {"pincode":farm.pincode,"address":farm.address};
+    }
+    else{
+        return {"pincode":user.pincode,"address":user.address};
+    }
+  });
+
+  
+//   for (farmIdx in finalPath) {
+//     if (farmIdx < validFarms.length) {
+//       const farmProducts = validFarms[farmIdx].products;
+
+//       farmProducts.forEach(async (productId) => {
+//         const product = await Product.findById(productId);
+//         if(product.name in requiredProducts && requiredProducts[product.name] > 0){
+//             const currQuantity = Math.min(product.stock, requiredProducts[product.name]);
+//             product.stock -= currQuantity;
+//             requiredProducts[product.name] -= currQuantity;
+//             await product.save();
+//         }
+//       });
+//     }
+//   }
+
+  return res.status(200).json({
+    success: true,
+    message: "Order Created Successfully",
+    finalPath,
+    min_dist
+  });
 });
 
+module.exports.myOrders = TryCatch(async (req, res, nect) => {
+  const { id } = req.query;
+  const user = await User.findById(id).populate("orders");
+
+  return res.status(200).json({
+    success: true,
+    orders: user.orders,
+  });
+});
